5ff178febe6cab2b6a08b826e3fe3ec8bf6503aa
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2025-07-30 13:15:29 +0200

    Made 2 new models: posts and categories, made and migrated

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Category;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $title,
+            'slug' => \Illuminate\Support\Str::slug($title),
+            'content' => fake()->paragraph(5),
+            'image' => fake()->imageUrl(640, 480, 'posts'),
+            'category_id' => Category::inRandomOrder->first()->id,
+            'user_id' => 1,
+            'published_at' => fake()->optional()->dateTime(),
+        ];
+    }
+}
