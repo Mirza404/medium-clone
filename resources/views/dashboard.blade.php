03378db4df108b801f10d8c372e5672145914fa6
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2025-07-30 16:47:20 +0200

    Pagination added on the controller

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,28 +10,51 @@
 
 
             <div class=" overflow-hidden shadow-sm sm:rounded-lg">
-                <ul
-                    class="flex flex-wrap text-sm font-medium text-center bg-white text-gray-500 dark:text-gray-400 p-4 justify-center">
+                <ul class="flex flex-wrap text-sm font-medium text-center bg-white text-gray-500 p-4 justify-center">
                     @foreach ($categories as $category)
                         <li class="me-2">
                             <a href="#"
-                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">{{ $category->name }}</a>
+                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 ">{{ $category->name }}</a>
                         </li>
                     @endforeach
                 </ul>
             </div>
 
-            <div class=" overflow-hidden shadow-sm sm:rounded-lg">
-                <ul
-                    class="flex flex-wrap text-sm font-medium bg-white text-center text-gray-500 dark:text-gray-400 p-4 justify-center mt-8">
-                    @foreach ($posts as $post)
-                        <li class="me-2">
-                            <a href="#"
-                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">{{ $post->title }}</a>
-                        </li>
-                    @endforeach
-                </ul>
+            <div class="mt-8 text-gray-900">
+                <div>
+                    <ul>
+                        @foreach ($posts as $post)
+                            <div class=" bg-white border border-gray-200 rounded-lg shadow-sm ">
+                                <a href="#">
+                                    <img class="rounded-t-lg" src="/docs/images/blog/image-1.jpg" alt="" />
+                                </a>
+                                <div class="p-5">
+                                    <a href="#">
+                                        <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                                            Noteworthy technology acquisitions 2021</h5>
+                                    </a>
+                                    <p class="mb-3 font-normal text-gray-700 ">Here are the biggest
+                                        enterprise technology acquisitions of 2021 so far, in reverse chronological
+                                        order.</p>
+                                    <a href="#"
+                                        class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 ">
+                                        Read more
+                                        <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true"
+                                            xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                                stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9" />
+                                        </svg>
+                                    </a>
+                                </div>
+                            </div>
+                        @endforeach
+                    </ul>
+
+                    
+                </div>
             </div>
         </div>
+        {{ $posts->links() }}
+    </div>
     </div>
 </x-app-layout>
