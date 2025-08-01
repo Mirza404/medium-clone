b410e61cee962592b5990eb2028077fad5da0d4e
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2025-08-01 14:10:02 +0200

    Endpoint name changed to dashboard

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/', [PostController::class, 'index'])->name('/');
+    Route::get('/', [PostController::class, 'index'])->name('dashboard');
     Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
     Route::post('/post', [PostController::class, 'store'])->name('post.store');
 });
