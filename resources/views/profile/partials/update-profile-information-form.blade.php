8f3be756f25826d9c936e63ca3a1884566d954a1
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2025-08-02 15:58:47 +0200

    Actual user image added, along with error catching if its absent

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -34,7 +34,7 @@
 
         @if ($user->image)
             <div class="mb-4">
-                <img src="{{ Storage::url($user->image) }}" alt="{{ $user->name }}" class="w-24 h-24 rounded-full">
+                <img src="{{ $user->imageUrl() }}" alt="{{ $user->name }}" class="w-24 h-24 rounded-full">
             </div>
         @endif
 
