f579eacbab709e215edef7a6506acd9bd94097dc
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2025-08-02 15:56:48 +0200

    Post item updated with links to post.show

--- resources/views/components/post-item.blade.php
+++ resources/views/components/post-item.blade.php
@@ -1,13 +1,12 @@
-
 <div class=" flex bg-white border border-gray-200 rounded-lg shadow-sm mb-8">
     <div class="p-5 flex-1">
-        <a href="#">
+        <a href="{{ route('post.show', ['username' => $post->user->username, 'post' => $post->slug]) }}">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900">
                 {{ $post->title }}</h5>
         </a>
         <p class="mb-3 font-normal text-gray-700 ">{{ Str::words($post->content, 20) }}
         </p>
-        <a href="#">
+        <a href="{{ route('post.show', ['username' => $post->user->username, 'post' => $post->slug]) }}">
             <x-primary-button>
                 Read more
                 <svg class="rtl:rotate-180 w-3.5 h-3.5 ms-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
@@ -18,8 +17,7 @@
             </x-primary-button>
         </a>
     </div>
-    <a href="#" class="">
-        <img class="rounded-t-lg w-48 h-full object-cover" src={{Storage::url($post->image)}}
-            alt="" />
+    <a href="{{ route('post.show', ['username' => $post->user->username, 'post' => $post->slug]) }}" class="">
+        <img class="rounded-t-lg w-48 h-full object-cover" src={{ Storage::url($post->image) }} alt="" />
     </a>
 </div>
