8475d92959088a72fca01709c802d91b7795c0e8
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2025-08-03 15:21:01 +0200

    Added a post route for following + manual format

--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,31 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PublicProfileController;
 use App\Http\Controllers\PostController;
+use App\Http\Controllers\FollowerController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/@{user}', [PublicProfileController::class, 'show'])->name('profile.show');
+Route::get('/@{user}', [PublicProfileController::class, 'show'])
+    ->name('profile.show');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/', [PostController::class, 'index'])->name('dashboard');
-    Route::get('/post/create', [PostController::class, 'create'])->name('post.create');
-    Route::post('/post', [PostController::class, 'store'])->name('post.store');
-    Route::get('/@{username}/{post:slug}', [PostController::class, 'show'])->name('post.show');});
+    Route::get('/', [PostController::class, 'index'])
+        ->name('dashboard');
+    Route::get('/post/create', [PostController::class, 'create'])
+        ->name('post.create');
+    Route::post('/post', [PostController::class, 'store'])
+        ->name('post.store');
+    Route::get('/@{username}/{post:slug}', [PostController::class, 'show'])
+        ->name('post.show');});
+    Route::post('/follow/{user:username', FollowerController::class, 'followUnfollow')
+        ->name('follow.unfollow');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
 });
 
 require __DIR__ . '/auth.php';
