b8c8beb50f1b6d88b6061eecbaa9c9aee4e9af05
Mirza Abdulahovic <mirzaabdulahovic1@gmail.com>
2026-03-03 15:00:51 +0100

    Add ReadingList model with relationships to posts and user

---
+++ app/Models/ReadingList.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ReadingList extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'slug',
+    ];
+
+    public function posts()
+    {
+        return $this->belongsToMany(Post::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
